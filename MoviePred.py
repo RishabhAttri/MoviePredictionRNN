88ac5dfaf78f772c978a53b3912c011f02032f69
RishabhAttri <rishabhattri2308@gmail.com>
2019-12-23 20:59:03 +0530

    Data Preprocessing done MB

--- MoviePred.py
+++ MoviePred.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import re
 
+
 #Import the Dataset
 dataset = pd.read_csv('train.csv')
 X = dataset.iloc[:,0].values
@@ -18,6 +19,7 @@ Y = dataset.iloc[:,1].values
 #Clean the Dataset
 def clean_text(text):
     text = re.sub(r"[-()~@<>+=|.?\"#;:,{}]", "", text)
+    text = re.sub(r"[the,a,an]" , "", text)
     return text
 
 clean_X = []
@@ -39,7 +41,7 @@ print(len(words2int))
 
 #Remove the words below threshold
 out_string = '<OUT>'
-threshold = 25
+threshold = 50
 wordsAboveThres2int = {}
 word_num = 0
 for word, value in words2int.items():
@@ -61,6 +63,46 @@ for sent in clean_X:
             clean_sent.append(out_string)
     cl_clean_X.append(clean_sent)
     
+#One Hot Y
+pos2one = {'pos' : 1, 'neg' : 0}
+binY = []
+for y in Y:
+    binY.append(pos2one[y])
 
+#Creating the embedding dictionary
+f = open('glove.6B.50d.txt', encoding='utf-8')
+word_to_50dVec = {}
+cnt = 0
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:],dtype='float')
+    word_to_50dVec[word] = coefs
+f.close()
 
+for L in cl_clean_X:
+    if L.__contains__('<OUT>'):
+        L.remove('<OUT>')
+cnt = 0
+for L in cl_clean_X:
+    if cnt < len(L):
+        cnt = len(L)
+print(cnt)
+    
+#train test split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(cl_clean_X, binY,test_size = 0.2)
+#The embedding layer
+dim = 50
+def embedding_output(X):
+    maxLen = 2200
+    X = np.asarray(X)
+    embedding_out = np.zeros((X.shape[0],maxLen,dim))
+    for sentIndex in range(X.shape[0]):
+        for wrdIndex in range(X[sentIndex].shape[0]):
+            embedding_out[sentIndex][wrdIndex] = word_to_50dVec[X[sentIndex][wrdIndex]]
+    return embedding_out
+
+embedding_matrix_train = embedding_output(X_train)
+embedding_matrix_test = embedding_output(X_test)
 
